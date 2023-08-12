ca424a306a71e9d09c1f80d022521966a89df18a
endriqon61 <endritshaqiri61@gmail.com>
2023-08-12 20:55:39 +0200

    initial

---
+++ src/app/notebook/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useState } from 'react'
+import styles from "./styles.module.css"
+
+const NoteBookEditor = () => {
+
+    const [insertText, setInsertText] = useState<string>("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+    return (
+        <>
+        <h1>Notebook</h1>
+        
+        <div className={styles.textContainer}>
+
+        <textarea onChange={(e) => {setInsertText(e.target.value)}} value={insertText} className={styles.textarea}>
+        </textarea>
+
+        <div className={styles.textResult}>
+            <p>
+                {insertText.length > 0 &&  "Krenar is gay + "}{insertText.toLocaleUpperCase()}
+            </p>
+        </div>
+
+        </div>
+        
+        </>
+    )
+}
+
+export default NoteBookEditor
